b924eb865e7fcf28cf424f9cc0039700d5711a03
Zoe <buckzoe5@gmail.com>
2022-02-04 15:35:21 -0500

    commit tree db2083e6c9ba629307f71e5c4d13c483a42fc6b6

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let newText = document.getElementById("text")
+    newText.textContent = "This is really cool!"
+    console.log(newText.textContent)
+})
